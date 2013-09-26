065ea862534e21d09ce1ff7806a988e3a7d0cf2b
spoonman1982 <farrellyjames@yahoo.ie>
2013-09-26 17:26:36 +0100

    mini_magick installed

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,5 +1,7 @@
 class News < ActiveRecord::Base
   attr_accessible :content, :news_image, :title
 
+  mount_uploader :news_image, NewsImageUploader
+
   @news_item = News.order("created_at").first
 end
